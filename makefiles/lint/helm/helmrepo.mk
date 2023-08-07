ba83b0a963653cc305ee60fa2cf3ab7930f24de9
Joey Armstrong <joey@opennetworking.org>
2023-08-07 12:27:31 -0400

    [VOL-5114] - bbsim version is stale
    Makefile
    .gitignore
    makefiles/lint/helm/include.mk
    makefiles/lint/helm/helmrepo.mk
    -------------------------------
      o Added convenience targets for cloning cord-charts-repo
      o helmrepo.mk added for interactive use so devs working
        locally can run the same code checks as jenkins commit hook.
    
    bbsim/Chart.yaml
    ----------------
       o Bump bbsim version, very stale (1.12.10 VS 1.15.2).
         https://github.com/opencord/bbsim/releases/tag/v1.15.2
         A new bbsim release build needed but get current first.
       o Bump version string as well, helm checks report 4.8.9
         already exists.
       o Another failure wrt duplicate voltha-stack was reported
         but get this patch in first for current status from jenkins
         then deal with downstream failures as git commit --amend.
    
    Change-Id: I8320f75ad48de02986d32e391b429e0ea4375b2b

---
+++ makefiles/lint/helm/helmrepo.mk
@@ -0,0 +1,59 @@
+# -*- makefile -*-
+# -----------------------------------------------------------------------
+# Copyright 2023 Open Networking Foundation (ONF) and the ONF Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------
+
+##-------------------##
+##---]  GLOBALS  [---##
+##-------------------##
+
+##-------------------##
+##---]  TARGETS  [---##
+##-------------------##
+.PHONY: lint-helmrepo
+
+ifndef NO-LINT-HELMREPO
+  lint : lint-helmrepo
+endif
+
+## -----------------------------------------------------------------------
+## Intent: Jenkins hook test
+## -----------------------------------------------------------------------
+lint-helmrepo-deps += cord-charts-repo
+lint-helmrepo-deps += lint-helm
+lint-helmrepo: $(lint-helmrepo-deps)
+	$(HIDE) $(MAKE) helm-repo-tools
+	helm-repo-tools/helmrepo.sh --no-publish
+
+## -----------------------------------------------------------------------
+## Intent: Dependency for lint-helmrepo
+## -----------------------------------------------------------------------
+cord-charts-repo:
+	git clone ssh://gerrit.opencord.org:29418/cord-charts-repo.git
+
+## -----------------------------------------------------------------------
+## Intent: Checkout lint tools
+## -----------------------------------------------------------------------
+helm-repo-tools:
+	git clone "https://gerrit.opencord.org/helm-repo-tools"
+
+## -----------------------------------------------------------------------
+## Intent: Revert to a pristine sandbox checkout
+## ----------------------------------------------------------------------
+sterile ::
+	$(RM) -r cord-charts-repo
+	$(RM) -r helm-repo-tools
+
+# [EOF]
